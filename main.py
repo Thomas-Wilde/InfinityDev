10f879ff8a8d6d0ecbc7d014d92b29c27a94bdb4
Lego Account <no@mail.de>
2019-11-16 13:26:43 +0100

    deleted unnecessary code from PID

--- main.py
+++ main.py
@@ -55,67 +55,7 @@ def followLine():
     print(u)
     previous_error = error
 
-
-
-# def followLine2():
-#   speed = 360/4  # deg/sec, [-1000, 1000]
-#   dt = 500       # milliseconds
-#   watch = StopWatch()
-  
-
-#   # PID tuning
-#   Kp = 1  # proportional gain
-#   Ki = 0  # integral gain
-#   Kd = 0  # derivative gain
-
-#   integral = 0
-#   previous_error = 0
-
-#   # initial measurment
-#   cols = getColors()
-#   target_value = cols[0] - cols[1]
-
-#   # Start the main loop
-#   while True:
-#       # Calculate steering using PID algorithm
-#       cols = getColors()
-#       diff = cols[0] - cols[1]
-#       error = target_value - diff
-#       integral += (error * dt)
-#       derivative = (error - previous_error) / dt
-
-#       # u zero:     on target,  drive forward
-#       # u positive: too bright, turn right
-#       # u negative: too dark,   turn left
-#       u = (Kp * error) + (Ki * integral) + (Kd * derivative)
-
-#       # limit u to safe values: [-1000, 1000] deg/sec
-#       if speed + abs(u) > 1000:
-#           if u >= 0:
-#               u = 1000 - speed
-#           else:
-#               u = speed - 1000
-
-#       # run motors
-#       if u >= 0:
-#           motor_l.run_time(speed-u, dt, Stop.COAST, False)
-#           motor_r.run_time(speed+u, dt, Stop.COAST, False)
-#           wait(dt)
-#       else:
-#           motor_l.run_time(speed+u, dt, Stop.COAST, False)
-#           motor_r.run_time(speed-u, dt, Stop.COAST, False)
-#           wait(dt)
-
-#       previous_error = error
-
-# # alignStart()
-# # driveDistance(15.5, 200.0)
-# # stopMotors()
-# # brick.sound.beep()
-# # turnRobot(90.0, 100.0)
-# # brick.sound.beep()
-# # driveDistance(87.0, 200.0)
-# # stopMotors()
-
 #driveDistance(200, 70)
 followLine()
+
+# change some code
