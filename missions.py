724b85e735cdfbdc77e53c0816e744a7ef626c9c
Lego Account <no@mail.de>
2020-01-10 16:07:51 +0100

    .

--- missions.py
+++ missions.py
@@ -7,45 +7,45 @@ def Task6():    # Missoin: Stau
   s2  = 113.0
   driveDistance(s1, 300.0)
   way += s1
-  way += searchLine(100.0, "right", "white") 
-  way += searchLine(100.0, "right", "black") 
-  turnRobot(15.0, 200.0)
+  way += searchLine(150.0, "right", "white") 
+  way += searchLine(150.0, "right", "black") 
+  turnRobot(15.0, 300.0)
   # ueber den Satz des Pythagoras errechnen wir, wie weit wir noch fahren muessen
   dist = math.sqrt(way*way - 23.0*23.0)
   print("to Go ***********************")
   print(s2 - dist)  
-  driveDistance(s2 - dist, 300.0, 0.0, 400.0)
-  driveDistance(-17.0, 300.0, -270.0, 500.0)
-  driveDistance(-7.5, 200.0, 0.0, 300.0)
+  driveDistance(s2 - dist, 350.0, 0.0, 400.0)
+  driveDistance(-17.0, 350.0, -270.0, 500.0)
+  driveDistance(-7.5, 250.0, 0.0, 300.0)
   alignBackward()
   brick.sound.beep()
-  searchLine(100.0, "right", "white", stop=False) 
-  searchLine(100.0, "right", "black", stop=False) 
-  searchLine(100.0, "right", "white")
+  searchLine(150.0, "right", "white", stop=False) 
+  searchLine(150.0, "right", "black", stop=False) 
+  searchLine(150.0, "right", "white")
   Task7() 
 
 #------------------------------------------------------------------------#
 def Task7():     # Mission: Schaukel
-  turnRobot(90.0, 200.0)
-  driveDistance(30.0 ,300.0, acc=400.0)
-  turnRobot(-90.0, 200.0)
+  turnRobot(90.0, 180.0)
+  driveDistance(32.0 ,300.0, acc=400.0)
+  turnRobot(-90.0, 180.0)
   alignBackward(200.0)
-  driveDistance(5.0, 100.0)
+  driveDistance(5.0, 130.0)
   # Werkzeug abklappen
   function_l.set_dc_settings(200.0, 0.0)
   function_l.reset_angle(0.0)
   while (abs(function_l.angle()) < 90.0):
     function_l.run_until_stalled(800.0)
-  turnRobot(90.0 , 200.0)
+  turnRobot(90.0 , 180.0)
   driveDistance(20.0, 300.0)
   Task8()
 #------------------------------------------------------------------------#
 def Task8():      # Mission: Fahrstuhl
   driveDistance(-15.0, 250.0, 0.0, 400.0)
-  turnRobot(-90.0, 200.0)
+  turnRobot(-90.0, 180.0)
   alignBackward(200.0)
-  driveDistance(26.0, 200.0, acc=300.0)
-  s = searchLine(100.0, "right", "black")
+  driveDistance(26.0, 220.0, acc=300.0)
+  s = searchLine(150.0, "right", "black")
   driveDistance(-2.0, 100.0)
   s -= 2.0
   b = 40.0*(20.0-s)/20.0
